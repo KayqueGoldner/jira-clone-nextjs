da3b58e1e8b81fb4935656676528a16a192a638b
Kayque Goldner <kayquegoldner@gmail.com>
2024-10-11 12:11:11 -0300

    custom components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,34 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button
-        variant="destructive"
-        size="sm"
-      >
-        Click me
+    <div className="p-5">
+      <Input />
+      <Button variant="primary" size="lg">
+        primary
       </Button>
-      <p className="text-red-500 font-semibold">
-        Hello World
-      </p>
+      <Button variant="secondary">
+        secondary
+      </Button>
+      <Button variant="destructive" size="xs">
+        destructive
+      </Button>
+      <Button variant="ghost">
+        ghost
+      </Button>
+      <Button variant="muted">
+        muted
+      </Button>
+      <Button variant="outline">
+        outline
+      </Button>
+      <Button variant="teritary">
+        teritary
+      </Button>
+      <Button disabled>
+        disabled
+      </Button>
     </div>
   );
 }
