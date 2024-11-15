d5cd1bdcfa617d770e78f1b637689f6d0ee37ec1
Kayque Goldner <kayquegoldner@gmail.com>
2024-11-15 15:45:06 -0300

    feat: data kanban

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -14,6 +14,7 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
   const workspaceId = useWorkspaceId();
@@ -98,7 +99,7 @@ export const TaskViewSwitcher = () => {
               value="kanban" 
               className="mt-0"
             >
-              {JSON.stringify(tasks)}
+              <DataKanban data={tasks?.documents || []} />
             </TabsContent>
             <TabsContent 
               value="calendar" 
